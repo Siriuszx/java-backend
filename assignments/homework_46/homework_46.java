53b573633638e6c714e051a94ec351d44b936dda
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-11-12 19:09:43 +0100

    Solve task with asterisk

--- assignments/homework_46/homework_46.java
+++ assignments/homework_46/homework_46.java
@@ -14,5 +14,24 @@ public class homework_46 {
         System.out.println(SetUtil.union(set1, set2));
         System.out.println(SetUtil.intersection(set1, set2));
         System.out.println(SetUtil.difference(set1, set2));
+
+        // ----------------------------------------------------------------------------------------------------------
+
+        String string = "abcdefj ab,cdf,";
+        System.out.println(getFirstUniqueChar(string));
+    }
+
+    public static char getFirstUniqueChar(String text) {
+        char[] charArr = text.replaceAll("\\s+", "").toCharArray();
+
+        for (int i = 0; i < charArr.length; i++) {
+            for (int j = 1; j < charArr.length; j++) {
+                if (charArr[i] == charArr[j] && i != j) break;
+                // Return character if character match wasn't found and the loop has reached its end
+                if (j == charArr.length - 1) return charArr[i];
+            }
+        }
+
+        return '.';
     }
 }
